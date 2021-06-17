e744edbf459fc2eac68010bc8ae8b8ecd9637070
Juan Sebastian Reyes Carreño <jsrc1990@hotmail.com>
2021-06-17 12:06:41 -0500

    Se agregan las clases PropertyChangedBase y Response

---
+++ TransversalLibrary/PropertyChangedBase.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TransversalLibrary
+{
+    /// <summary>
+    /// Define la clase base para notificar que las propiedades de las clases heredadas han cambiado su valor
+    /// </summary>
+    public class PropertyChangedBase
+    {
+        #region PROPERTY CHANGED
+
+        /// <summary>
+        /// Define el evento que notifica que la propiedad ha cambiado
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notifica que el valor de la propiedad especificada de esta clase ha cambiado
+        /// </summary>
+        public void OnPropertyChanged([CallerMemberName] string Name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Name));
+        }
+
+        #endregion
+    }
+}
