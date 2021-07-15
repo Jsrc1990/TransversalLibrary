c4628d04c2500e4720749f0ac12b5722293543bc
Juan Sebastian Reyes Carreño <jsrc1990@hotmail.com>
2021-07-15 00:43:32 -0500

    Agregado el proyecto TransversalLibrary.Standard

---
+++ TransversalLibrary.Standard/Response.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace TransversalLibrary.Standard
+{
+    /// <summary>
+    /// Define el response de la función
+    /// </summary>
+    /// <typeparam name="T">Define el tipo genérico</typeparam>
+    public class Response<T> : PropertyChangedBase
+    {
+        #region HTTP STATUS CODE
+
+        /// <summary>
+        /// Define el código de estado Http
+        /// </summary>
+        private HttpStatusCode? _HttpStatusCode;
+
+        /// <summary>
+        /// Obtiene o establece el código de estado Http
+        /// </summary>
+        public HttpStatusCode? HttpStatusCode
+        {
+            get
+            {
+                return _HttpStatusCode;
+            }
+            set
+            {
+                _HttpStatusCode = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region DATA
+
+        /// <summary>
+        /// Define el objeto de respuesta
+        /// </summary>
+        private T _Data = default(T);
+
+        /// <summary>
+        /// Obtiene o establece el objeto de respuesta
+        /// </summary>
+        public T Data
+        {
+            get
+            {
+                return _Data;
+            }
+            set
+            {
+                _Data = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region EXCEPTION
+
+        /// <summary>
+        /// Obtiene o establece la excepción
+        /// </summary>
+        public Exception Exception { get; set; }
+
+        #endregion
+
+        #region ERRORS
+
+        /// <summary>
+        /// Obtiene los errores
+        /// </summary>
+        public List<string> Errors { get; } = new List<string>();
+
+        #endregion
+    }
+}
